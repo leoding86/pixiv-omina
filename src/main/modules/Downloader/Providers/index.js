013c234e398c877a1d50283a31901ae7405a0f7a
Leo Ding <leoding86@gmail.com>
2020-09-05 22:46:12 +0800

    update

--- src/main/modules/Downloader/Providers/index.js
+++ src/main/modules/Downloader/Providers/index.js
@@ -3,11 +3,15 @@ import PixivGeneralArtworkProvider from './Pixiv/GeneralArtworkProvider';
 import PixivMangaProvider from './Pixiv/MangaProvider';
 import PixivIllustrationProvider from './Pixiv/IllustrationProvider';
 import PixivUgoiraProvider from './Pixiv/UgoiraProvider';
+import PixivComicEpisodeProvider from './PixivComic/EpisodeProvider';
+import PixivComicWorkProvider from './PixivComic/WorkProvider';
 
 export {
   PixivUserProvider,
   PixivGeneralArtworkProvider,
   PixivMangaProvider,
   PixivIllustrationProvider,
-  PixivUgoiraProvider
+  PixivUgoiraProvider,
+  PixivComicEpisodeProvider,
+  PixivComicWorkProvider
 }
