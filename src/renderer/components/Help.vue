46b248a3aeff6ff9847c5991837d69ca76932e01
Leo Ding <leoding86@gmail.com>
2020-09-05 22:48:17 +0800

    add `Help` component

---
+++ src/renderer/components/Help.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app-help">
+    <p><strong>{{ $t('_pixiv_valid_rename_placeholders') }}: </strong></p>
+    <p>%id%, %title%, %user_id%, %user_name%, %page_num%, %year%, %month%, %day%</p>
+    <el-divider></el-divider>
+    <p><strong>{{ $t('_pixiv_comic_valid_rename_placeholders') }}: </strong></p>
+    <p>%id%, %work_id%, %sub_title%, %numbering_title%, $title%, %user_name%, %page_num%</p>
+    <el-divider></el-divider>
+    <p><strong>{{ $t('_you_can_add_forward_slashs_to_save_images_in_subfolder_eg') }}: </strong></p>
+    <p>%user_id%/%title%</p>
+  </div>
+</template>
