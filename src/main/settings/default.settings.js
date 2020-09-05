470dd0420ff83ce04f059c9baa387bb541165dfe
Leo Ding <leoding86@gmail.com>
2020-09-05 22:43:50 +0800

    add setting `pixivComicWorkRename`

--- src/main/settings/default.settings.js
+++ src/main/settings/default.settings.js
@@ -12,6 +12,8 @@ function defaultSetting() {
 
     ugoiraRename: '%id%_%title%',
 
+    pixivComicWorkRename: '%user_name%/%work_id%_%work_title%/%number_title%%sub_title%/p%page_num%',
+
     overwriteMode: 'skip',
 
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3964.0 Safari/537.36',
