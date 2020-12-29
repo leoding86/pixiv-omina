5f2fca0c85f177e5c87dea39cedfedf7d2aad29a
leoding86 <leoding86@gmail.com>
2020-12-29 11:46:12 +0800

    update

--- src/main/global.js
+++ src/main/global.js
@@ -1,5 +1,6 @@
 import DebugService from '@/services/DebugService';
 
 const debug = DebugService.getService();
+const isDev = process.argv.indexOf('--dev') > 0;
 
-export { debug };
+export { debug, isDev };
