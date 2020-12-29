82ddbbfd6a16352ead16403fd200cfbf37c69077
leoding86 <leoding86@gmail.com>
2020-12-29 16:47:31 +0800

    cache and restore window size and position

--- src/main/modules/WindowManager.js
+++ src/main/modules/WindowManager.js
@@ -1,12 +1,16 @@
-import { BrowserWindow } from 'electron'
-import { format as formatUrl } from 'url'
-import * as path from 'path'
+import { app, BrowserWindow } from 'electron';
+import { format as formatUrl } from 'url';
+import * as path from 'path';
+import fs from 'fs-extra';
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 class WindowManager {
   constructor() {
     this.windows = {};
+    this.windowResizeTimeouts = {};
+    this.cacheFile = path.join(app.getPath('userData'), '.windows');
+    this.initialCacheFile();
   }
 
   static globalPartition = null;
@@ -52,6 +56,21 @@ class WindowManager {
   }
 
   /**
+   * @returns {void}
+   */
+  initialCacheFile() {
+    if (!fs.existsSync(this.cacheFile)) {
+      fs.writeJSONSync(this.cacheFile, {});
+    } else {
+      try {
+        fs.readJSONSync(this.cacheFile);
+      } catch(e) {
+        fs.writeJSONSync(this.cacheFile, {});
+      }
+    }
+  }
+
+  /**
    *
    * @param {string} name
    * @param {Electron.BrowserWindowConstructorOptions} args
@@ -64,6 +83,18 @@ class WindowManager {
 
     let window = new BrowserWindow(args);
     let url;
+
+    this.appendWindow(name, window);
+
+    this.restoreWindowBounds(name);
+
+    window.on('resize', () => {
+      this.cacheWindowBounds(name);
+    });
+
+    window.on('move', () => {
+      this.cacheWindowBounds(name);
+    });
 
     if (isDevelopment) {
       url = `http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}?target=${name}`
@@ -79,8 +110,6 @@ class WindowManager {
 
     window.loadURL(url, loadUrlArgs)
 
-    this.appendWindow(name, window)
-
     return window
   }
 
@@ -90,7 +119,8 @@ class WindowManager {
    * @param {Electron.BrowserWindow} window
    */
   appendWindow(name, window) {
-    this.windows[name] = window
+    this.windows[name] = window;
+    this.windowResizeTimeouts[name] = null;
   }
 
   /**
@@ -104,6 +134,69 @@ class WindowManager {
     }
 
     return this.windows[name];
+  }
+
+  /**
+   *
+   * @param {String} name Window name
+   */
+  getWindowResizeTimeout(name) {
+    return this.windowResizeTimeouts[name];
+  }
+
+  /**
+   *
+   * @param {String} name
+   * @param {Function} callback
+   * @param {Number} timeout
+   */
+  setWindowResizeTimeout(name, callback, timeout) {
+    this.windowResizeTimeouts[name] = setTimeout(callback.bind(this), timeout);
+  }
+
+  /**
+   *
+   * @param {String} name Window name
+   */
+  cacheWindowBounds(name) {
+    let timeout = this.getWindowResizeTimeout(name);
+
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    this.setWindowResizeTimeout(name, () => {
+      let cache = fs.readJSONSync(this.cacheFile);
+      let window = this.getWindow(name);
+
+      if (!cache[name]) {
+        cache[name] = {};
+      }
+
+      cache[name] = {
+        isMaximized: window.isMaximized(),
+        bounds: window.getBounds()
+      }
+
+      fs.writeJSONSync(this.cacheFile, cache);
+    }, 1500);
+  }
+
+  /**
+   *
+   * @param {String} name Window name
+   */
+  restoreWindowBounds(name) {
+    let window = this.getWindow(name);
+    let cache = fs.readJSONSync(this.cacheFile);
+
+    if (cache[name]) {
+      if (cache[name].isMaximized) {
+        window.maximize();
+      } else if (cache[name].bounds) {
+        window.setBounds(cache[name].bounds);
+      }
+    }
   }
 }
 
