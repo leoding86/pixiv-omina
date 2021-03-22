a34571022df78b48336650df4d5243bba2285b1b
leoding86 <leoding86@gmail.com>
2021-03-22 18:33:42 +0800

    add basic BaseProvider

---
+++ src/main/modules/Downloader/Providers/BaseProvider.js
@@ -0,0 +1,57 @@
+import Request from '@/modules/Request';
+
+class BaseProvider {
+
+  /**
+   * @param {object} options
+   * @param {string} options.url
+   * @param {object} options.context
+   * @param {string|number} options.context.id
+   */
+  constructor({ url, context }) {
+    /**
+     * @type {string}
+     */
+    this.url = url;
+
+    /**
+     * @type {Object}
+     */
+    this.context = context;
+
+    /**
+     * @type {Request}
+     */
+    this.request = null;
+
+    /**
+     * @type {string}
+     */
+    this.providerName = 'other';
+
+    /**
+     * @type {boolean}
+     */
+    this.provideMultipleDownloaders = false;
+  }
+
+  /**
+   * Get downloader is provided by the provider
+   * @param {Object} options
+   * @throws {Error}
+   */
+  getDownloader(options) {
+    throw new Error('Method "getDownloader" hasn\'t been implemented');
+  }
+
+  /**
+   * Get downloader are provided by the provider
+   * @param {Object} options
+   * @throws {Error}
+   */
+  getDownloaders(options) {
+    throw new Error('Method "getDownloaders" hasn\'t been implemented');
+  }
+}
+
+export default BaseProvider;
