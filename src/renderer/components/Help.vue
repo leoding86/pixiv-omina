cb66d42afff34ccc1b425eb286fd29b8cab6a3db
leoding86 <leoding86@gmail.com>
2021-03-22 18:52:34 +0800

    update Help

--- src/renderer/components/Help.vue
+++ src/renderer/components/Help.vue
@@ -3,6 +3,9 @@
     <p><strong>{{ $t('_pixiv_valid_rename_placeholders') }}: </strong></p>
     <p>%id%, %title%, %user_id%, %user_name%, %page_num%, %year%, %month%, %day%</p>
     <el-divider></el-divider>
+    <p><strong>{{ $t('_pixiv_novel_valid_rename_placeholder') }}</strong></p>
+    <p>%id%, %title%, %user_id%, %user_name%, %year%, %month%, %day%</p>
+    <el-divider></el-divider>
     <p><strong>{{ $t('_pixiv_comic_valid_rename_placeholders') }}: </strong></p>
     <p>%id%, %work_id%, %sub_title%, %number_title%, $title%, %user_name%, %page_num%</p>
     <el-divider></el-divider>
