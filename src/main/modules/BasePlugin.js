8755f8ce2d3914a386518ea3273f070c84d9c9fb
leoding86 <leoding86@gmail.com>
2021-03-24 21:23:59 +0800

    add BasePlugin

---
+++ src/main/modules/BasePlugin.js
@@ -0,0 +1,59 @@
+class BasePlugin {
+  /**
+   * @constructor
+   */
+  constructor() {
+    /**
+     * @property {RegExp[]}
+     */
+    this.patterns = [];
+
+    /**
+     * @property {string}
+     */
+    this.loginUrl = null;
+
+    /**
+     * @property {string}
+     */
+    this.icon = null;
+
+    /**
+     * @property {string}
+     */
+    this.title = null;
+
+    /**
+     * @property {RegExp[]}
+     */
+    this.requestUrlPatterns = [];
+  }
+
+  /**
+   * Create a provider
+   * @param {{ url: string, context: *}} param0
+   */
+  createProvider({ url, context }) {
+    throw new Error('Method createProvider has not been implemented');
+  }
+
+  /**
+   * Override request headers if necessary
+   * @param {{ url: string, context: *, requestHeaders: object }} args
+   * @returns {object}
+   */
+  requestHeadersOverrider({ url, context, requestHeaders }) {
+    return requestHeaders;
+  }
+
+  /**
+   * Override response header if necessary
+   * @param {{ url: string, context: object, responseHeaders: object }} args
+   * @returns {object}
+   */
+  responseHeadersOverrider({ url, context, responseHeaders }) {
+    return responseHeaders;
+  }
+}
+
+export default BasePlugin;
