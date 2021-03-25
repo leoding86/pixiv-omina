d7f2eb118a558c81ddc69229262714b818b255de
leoding86 <leoding86@gmail.com>
2021-03-25 17:50:40 +0800

    add query locale

--- src/renderer/LoginEntry.js
+++ src/renderer/LoginEntry.js
@@ -1,16 +1,34 @@
 import './styles/app.scss';
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
 import App from './components/Login';
+import locales from '../locales';
 
 class LoginEntry {
-  constructor({ loginUrl = null }) {
+  constructor({ loginUrl = null, locale = null }) {
+    Vue.use(VueI18n);
+
+    const i18n = new VueI18n({
+      locale: 'en',
+      messages: locales
+    });
+
+    if (locale) {
+      i18n.locale = locale;
+    }
+
     this.app = new Vue({
+      i18n,
+
       el: '#app',
+
       data() {
         return {
-          loginUrl
+          loginUrl,
+          locale
         }
       },
+
       render: h => h(App)
     });
   }
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,10 +2,11 @@
 
 const url = window.location.href;
 
-const matches = url.match(/target=([^&]+)(&login_url=([^&]+))?$/)
+const matches = url.match(/target=([^&]+)(&login_url=(?<login_url>[^&]+))?(&locale=(?<locale>[^&]+))?$/)
 
 let name = '',
-    loginUrl = '';
+    loginUrl = null,
+    locale = null;
 
 window.app;
 
@@ -13,14 +14,18 @@ if (matches[1]) {
   name = matches[1].slice(0, 1).toUpperCase() + matches[1].slice(1);
 }
 
-if (matches[3]) {
-  loginUrl = decodeURIComponent(matches[3]);
+if (matches.groups['login_url']) {
+  loginUrl = decodeURIComponent(matches.groups['login_url']);
 }
-console.log(matches);
+
+if (matches.groups['locale']) {
+  locale = matches.groups['locale'];
+}
+
 import(
   /* webpackInclude: /Entry\.js$/ */
   `./${name}Entry`
 ).then(({ default: Entry}) => {
-  let entry = new Entry({ loginUrl });
+  let entry = new Entry({ loginUrl, locale });
   window.app = entry.app;
 });
