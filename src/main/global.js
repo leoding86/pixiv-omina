35a8d951349236a8b257306fd29405f23acb5f89
leoding86 <leoding86@gmail.com>
2021-06-21 17:33:45 +0800

    expose md5 and parser to omina.utils

--- src/main/global.js
+++ src/main/global.js
@@ -1,5 +1,11 @@
 import DebugService from '@/services/DebugService';
+import Exposer from '@/modules/Exposer';
+import md5 from 'md5';
+import { parser } from 'node-html-parser';
 
 const debug = DebugService.getService();
+
+Exposer.expose('omina.utils.parser', parser);
+Exposer.expose('omina.utils.md5', md5);
 
 export { debug };
