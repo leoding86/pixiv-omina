6784f220132641c04337a8437b4a6cbb7efe960c
leoding86 <leoding86@gmail.com>
2021-06-23 21:08:12 +0800

    update

--- src/main/global.js
+++ src/main/global.js
@@ -1,11 +1,11 @@
 import DebugService from '@/services/DebugService';
 import Exposer from '@/modules/Exposer';
 import md5 from 'md5';
-import { parser } from 'node-html-parser';
+import { parse } from 'node-html-parser';
 
 const debug = DebugService.getService();
 
-Exposer.expose('omina.utils.parser', parser);
+Exposer.expose('omina.utils.parse', parse);
 Exposer.expose('omina.utils.md5', md5);
 
 export { debug };
