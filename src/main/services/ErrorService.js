db49195609e85918a04a8ba796f98a1649a97d83
leoding86 <leoding86@gmail.com>
2021-07-02 17:01:53 +0800

    add ErrorService for handling exceptions

---
+++ src/main/services/ErrorService.js
@@ -0,0 +1,56 @@
+import BaseService from './BaseService';
+import WindowManager from '@/modules/WindowManager';
+import {
+  debug
+} from '@/global';
+
+class ErrorService extends BaseService {
+  /**
+   * @property
+   * @type {ErrorService}
+   */
+  static instance;
+
+  /**
+   * @property
+   * @type {string}
+   */
+  static channel = 'error-service';
+
+  constructor() {
+    super();
+
+    process.on('uncaughtException', this.handleException.bind(this));
+  }
+
+  /**
+   * @returns {ErrorService}
+   */
+  static getService() {
+    if (!ErrorService.instance) {
+      ErrorService.instance = new ErrorService();
+    }
+
+    return ErrorService.instance;
+  }
+
+  /**
+   * Get renderer response channel
+   * @param {string} name
+   */
+  responseChannel(name) {
+    return ErrorService.channel + `:${name}`;
+  }
+
+  /**
+   * Handle exception
+   * @param {Error} error
+   */
+  handleException(error) {
+    debug.log(error);
+
+    WindowManager.getWindow('app').webContents.send(this.responseChannel('error'), error.message);
+  }
+}
+
+export default ErrorService;
