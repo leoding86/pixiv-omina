d8bded2e55e2b56655eb1d4d08ed98ac8d321aa9
leoding86 <leoding86@gmail.com>
2021-07-08 21:27:02 +0800

    add ScheduleTask

---
+++ src/main/modules/ScheduleTask.js
@@ -0,0 +1,17 @@
+class ScheduleTask {
+  /**
+   * @returns {Promise<void,Error>}
+   */
+  start() {
+    throw new Error('Method start has not been implemented');
+  }
+
+  /**
+   * @returns {Promise<void,Error>}
+   */
+  stop() {
+    throw new Error('Method stop has not been implemented');
+  }
+}
+
+export default ScheduleTask;
