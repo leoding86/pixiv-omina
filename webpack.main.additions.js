16ae0bc388dd55fcaaa791927b550d570dcc1d37
leoding86 <leoding86@gmail.com>
2021-07-20 12:23:17 +0800

    update webpack addition config

--- webpack.main.additions.js
+++ webpack.main.additions.js
@@ -1,5 +1,6 @@
 module.exports = {
   entry: {
-    UgoiraDownloaderGifEncoderWorker: './src/main/workers/UgoiraDownloader.GifEncoder.worker.js'
+    UgoiraDownloaderGifEncoderWorker: './src/main/workers/UgoiraDownloader.GifEncoder.worker.js',
+    UgoiraDownloaderGifEncoderFrameWorker: './src/main/workers/UgoiraDownloader.GifEncoder.FrameWorker.js'
   }
 }
